039ed9a42b78581f31b839e49c91968d08d18392
“00Amit0” <amit836961@gmail.com>
2022-06-10 02:15:25 +0530

    Welcome to Binary Search Tree

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's check what's binary search tree");
+        }
+    }
+}
