718250581341455ff942d13c125de4fe9439cdfd
“00Amit0” <amit836961@gmail.com>
2022-06-10 02:20:04 +0530

    Adding elements in BST

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Let's check what's binary search tree");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
         }
     }
 }
